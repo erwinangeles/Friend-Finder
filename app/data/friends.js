558707ba74802c70f22d5e329da1b5bd236a4d0e
Erwin Angeles <erwin@eang.media>
2019-10-27 19:16:51 -0500

    updating friends.js to include more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -21,6 +21,55 @@ var friendsArray = [
             4,
             1
           ]
+      },
+      {
+        "name":"Jose",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            4,
+            1,
+            1,
+            2,
+            5,
+            4,
+            2,
+            1,
+            3,
+            1
+          ]
+      },
+      {
+        "name":"Mark",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+      ,
+      {
+        "name":"Jerry",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            2,
+            1,
+            3,
+            4,
+            1,
+            1,
+            3,
+            1,
+            2,
+            1
+          ]
       }
       
   ];
